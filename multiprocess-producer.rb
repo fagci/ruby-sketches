e11683876233710dfd9f2009817b950944cdfce4
Mikhail Yudin <fagci.nsk@gmail.com>
2022-10-16 16:20:00 +0700

    refactor

--- multiprocess-producer.rb
+++ multiprocess-producer.rb
@@ -42,17 +42,17 @@ module Concurrent
 
     def spawn_workers(count, &block)
       proc_count, thr_count = Parallel.calc_resources count
-      proc_count.times do
+      proc_count.times do |i|
         ::Process.fork do
-          spawn_threads(thr_count, &block).each(&:join)
+          spawn_threads(thr_count, i, &block).each(&:join)
         end
       end
     end
 
-    def spawn_threads(count, &block)
-      count.times.map do
+    def spawn_threads(count, proc_i, &block)
+      count.times.map do |i|
         Thread.new do
-          @channel.instance_eval(&block)
+          @channel.instance_exec(proc_i, i, &block)
         end
       end
     end
@@ -68,10 +68,18 @@ module Concurrent
   end
 end
 
+def time
+  Process.clock_gettime(Process::CLOCK_MONOTONIC)
+end
+
 mp = Concurrent::Parallel.new do |data|
   puts data
 end
 
-mp.work(8) do
-  write sleep(rand(5))
+start = time
+mp.work(2) do |proc_i, thr_i|
+  2.times do |i|
+    sleep_time = sleep(rand(5))
+    write "[#{format('%6.3f', (time - start))}] proc#{proc_i} thr#{thr_i} i#{i} sleep(#{sleep_time})"
+  end
 end
